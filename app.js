543461484fd9aa93d3dec74150e47fcc88f7187b
Luanzura <luanmaximus@gmail.com>
2024-03-15 18:09:54 -0300

    refeito completamente

--- app.js
+++ app.js
@@ -1,38 +1,97 @@
-function gerarDeclaracao() {
-    const camposObrigatorios = ['nome', 'rua', 'numero', 'cidade', 'uf', 'cep', 'conteudo', 'quantidade', 'valor', 'peso'];
+function gerarDeracao() {
+    // Preencher os dados na declaração
+    // REMETENTE
+    document.getElementById('r-re-nome').innerHTML = document.getElementById('re-nome').value;
+    document.getElementById('r-re-rua').innerHTML = document.getElementById('re-rua').value;
+    document.getElementById('r-re-numero').innerHTML = document.getElementById('re-numero').value;
+    document.getElementById('r-re-cidade').innerHTML = document.getElementById('re-cidade').value;
+    document.getElementById('r-re-estado').innerHTML = document.getElementById('re-estado').value;
+    document.getElementById('r-re-cep').innerHTML = document.getElementById('re-cep').value;
+
+    // DESTINATARIO
+    document.getElementById('r-de-nome').innerHTML = document.getElementById('de-nome').value;
+    document.getElementById('r-de-rua').innerHTML = document.getElementById('de-rua').value;
+    document.getElementById('r-de-numero').innerHTML = document.getElementById('de-numero').value;
+    document.getElementById('r-de-cidade').innerHTML = document.getElementById('de-cidade').value;
+    document.getElementById('r-de-estado').innerHTML = document.getElementById('de-estado').value;
+    document.getElementById('r-de-cep').innerHTML = document.getElementById('de-cep').value;
+
+    //CONTEUDO
+    document.getElementById('r-conteudo').innerHTML = document.getElementById('conteudo').value;
+    document.getElementById('r-quantidade').innerHTML = document.getElementById('quantidade').value;
+    document.getElementById('r-valor').innerHTML = "R$" + document.getElementById('valor').value;
+    document.getElementById('r-peso').innerHTML = document.getElementById('peso').value + "Kg";
+    document.getElementById('data').innerHTML = dataAtual();
+    
+    // Imprimir apenas se todos os campos obrigatórios estiverem preenchidos
+    printDeclaracao()
+    window.print();
+};
+
+function dataAtual() {
+    let data = new Date();
+    return data.getDate() +  "/"  + (data.getMonth() + 1) +  "/"  + data.getFullYear();
+};
+
+function destinatarioIr() {
+    const camposObrigatorios = ['re-nome', 're-cep', 're-rua', 're-numero', 're-cidade', 're-estado'];
     let todosPreenchidos = true;
 
     // Verificar se todos os campos obrigatórios estão preenchidos
     camposObrigatorios.forEach(function(campo) {
         if (document.getElementById(campo).value === "") {
             todosPreenchidos = false;
-            return;
+            return; // Se encontrar um campo vazio, sai do loop
         }
     });
 
-    if (!todosPreenchidos) {
+    // Se todos os campos obrigatórios estiverem preenchidos, oculta "remente" e exibe "destinatario"
+    if (todosPreenchidos) {
+        document.getElementById("remente").classList.add("d-none");
+        document.getElementById("destinatario").classList.remove("d-none");
+    } else {
         alert("Por favor, preencha todos os campos obrigatórios.");
-        return; // Não continue se algum campo estiver vazio
-    }
+    };
+};
 
-    // Preencher os dados na declaração
-    document.getElementById('nome_destinatario').innerHTML = document.getElementById('nome').value;
-    document.getElementById('rua_destinatario').innerHTML = document.getElementById('rua').value;
-    document.getElementById('numero_destinatario').innerHTML = document.getElementById('numero').value;
-    document.getElementById('cidade_destinatario').innerHTML = document.getElementById('cidade').value;
-    document.getElementById('uf_destinatario').innerHTML = document.getElementById('uf').value;
-    document.getElementById('cep_destinatario').innerHTML = document.getElementById('cep').value;
-    document.getElementById('conteudo_destinatario').innerHTML = document.getElementById('conteudo').value;
-    document.getElementById('quantidade_destinatario').innerHTML = document.getElementById('quantidade').value;
-    document.getElementById('valor_destinatario').innerHTML = document.getElementById('valor').value;
-    document.getElementById('peso_destinatario').innerHTML = document.getElementById('peso').value;
-    document.getElementById('data').innerHTML = dataAtual();
-    
-    // Imprimir apenas se todos os campos obrigatórios estiverem preenchidos
-    window.print();
-} 
+function destinatarioVoltar(){
+    document.getElementById("destinatario").classList.add("d-none");
+    document.getElementById("remente").classList.remove("d-none");
+};
 
-function dataAtual() {
-    let data = new Date();
-    return data.getDate() +  "/"  + (data.getMonth() + 1) +  "/"  + data.getFullYear();
+function remetenteIr() {
+    const camposObrigatorios = ['de-nome', 'de-cep', 'de-rua', 'de-numero', 'de-cidade', 'de-estado'];
+    let todosPreenchidos = true;
+
+    // Verificar se todos os campos obrigatórios estão preenchidos
+    camposObrigatorios.forEach(function(campo) {
+        if (document.getElementById(campo).value === "") {
+            todosPreenchidos = false;
+            return; // Se encontrar um campo vazio, sai do loop
+        }
+    });
+
+    // Se todos os campos obrigatórios estiverem preenchidos, oculta "remente" e exibe "destinatario"
+    if (todosPreenchidos) {
+        document.getElementById("destinatario").classList.add("d-none");
+        document.getElementById("declaracao-conteudo").classList.remove("d-none");
+    } else {
+        alert("Por favor, preencha todos os campos obrigatórios.");
+    };
+};
+
+function rementeVoltar(){
+    document.getElementById("declaracao-conteudo").classList.add("d-none");
+    document.getElementById("destinatario").classList.remove("d-none");
+};
+
+
+
+function printDeclaracao(){
+    document.getElementById("declaracao-conteudo").classList.add("d-none");
+    document.getElementById("print").classList.remove("d-none");
 }
+
+
+
+
--- cep.js
+++ cep.js
@@ -1,7 +1,7 @@
 function buscaCep() {
-    let cep = document.getElementById('cep').value;
-    if (cep !== "") {
-        let url = "https://viacep.com.br/ws/" + cep + "/json/";
+    let reCep = document.getElementById('re-cep').value;
+    if (reCep !== "") {
+        let url = "https://viacep.com.br/ws/" + reCep + "/json/";
 
         let req = new XMLHttpRequest();
         req.open("GET", url);
@@ -10,9 +10,33 @@ function buscaCep() {
         req.onload = function () {
             if (req.status === 200) {
                 let endereco = JSON.parse(req.response);
-                document.getElementById('rua').value = endereco.logradouro;
-                document.getElementById('cidade').value = endereco.localidade;
-                document.getElementById('uf').value = endereco.uf;
+                document.getElementById('re-rua').value = endereco.logradouro;
+                document.getElementById('re-cidade').value = endereco.localidade;
+                document.getElementById('re-estado').value = endereco.uf;
+            }
+            else if (req.status === 400) {
+                alert("Cep inválido");
+            }
+            else {
+                alert("Erro ao fazer a requisição")
+            }
+        }
+    }
+
+    let deCep = document.getElementById('de-cep').value;
+    if (deCep !== "") {
+        let url = "https://viacep.com.br/ws/" + deCep + "/json/";
+
+        let req = new XMLHttpRequest();
+        req.open("GET", url);
+        req.send();
+
+        req.onload = function () {
+            if (req.status === 200) {
+                let endereco = JSON.parse(req.response);
+                document.getElementById('de-rua').value = endereco.logradouro;
+                document.getElementById('de-cidade').value = endereco.localidade;
+                document.getElementById('de-estado').value = endereco.uf;
             }
             else if (req.status === 400) {
                 alert("Cep inválido");
@@ -25,6 +49,8 @@ function buscaCep() {
 }
 
 window.onload = function () {
-    let txtCep = document.getElementById("cep");
-    txtCep.addEventListener("input", buscaCep);
+    let txtReCep = document.getElementById("re-cep");
+    txtReCep.addEventListener("input", buscaCep);
+    let txtDeCep = document.getElementById("de-cep");
+    txtDeCep.addEventListener("input", buscaCep);
 }
